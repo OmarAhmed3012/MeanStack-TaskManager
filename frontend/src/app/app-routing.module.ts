8b1d36424c772294fdae0dab4d13f2314d6b71e4
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-04-03 00:12:56 +0200

    Update an delete Tasks and Lists

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
+import { EditListComponent } from './pages/edit-list/edit-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { EditTaskComponent } from './pages/edit-task/edit-task.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'lists', pathMatch: 'full' },
   { path: 'new-list', component: NewListComponent },
+  { path: 'edit-list/:listId', component: EditListComponent },
+  { path: 'lists/:listId/edit-task/:taskId', component: EditTaskComponent },
   { path: 'new-task', component: NewTaskComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -14,8 +14,22 @@ export class TaskService {
     return this.webReqService.post('lists', { title });
   }
 
+  updateList(id: string, title: string) {
+    // We want to send a web request to update a list
+    return this.webReqService.patch(`lists/${id}`, { title });
+  }
+
+  updateTask(listId:string, taskId: string, title: string) {
+    // We want to send a web request to update a list
+    return this.webReqService.patch(`lists/${listId}/tasks/${taskId}`, { title });
+  }
+
   getLists() {
     return this.webReqService.get('lists')
+  }
+
+  deleteList(id: string) {
+    return this.webReqService.delete(`lists/${id}`)
   }
 
   createTask(title: string, listId: string) {
@@ -32,5 +46,11 @@ export class TaskService {
       completed: !task.completed
     });
 }
+
+deleteTask(listId: string, taskId: string) {
+  return this.webReqService.delete(`lists/${listId}/tasks/${taskId}`)
+}
+
+
 
 }
